072c356683ebf09fdfd2404c66bdbd312a14f0dd
SvelteCondor <SvelteCondor@gmail.com>
2023-04-15 15:51:33 +0200

    MobileInvoice

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -8,7 +8,7 @@
     type="text"
     name="search"
     placeholder="Search by key-Word"
-    class="w-72 border-b-2 border-dashed border-b-pastelPurple bg-transparent font-sansSerif text-x1 text-black outline-none focus:border-b-lavenderIndigo focus:border-solid"
+    class="w-full md:w-[14rem] border-b-2 border-dashed border-b-pastelPurple bg-transparent font-sansSerif text-base lg:text-xl text-black outline-none focus:border-b-lavenderIndigo focus:border-solid"
   />
-  <button class="font-sansSerif text-x1 font-black text-pastelPurple">Search</button>
+  <button class="font-sansSerif text-lg lg:text-xl font-black text-pastelPurple">Search</button>
 </div>
--- src/routes/(dashboard)/invoices/+page.svelte
+++ src/routes/(dashboard)/invoices/+page.svelte
@@ -10,14 +10,16 @@
   <title>Invoices - The Dollar Holler</title>
 </svelte:head>
 
-<div class="flex justify-between items-center mb-16">
+<div
+  class="flex flex-col-reverse md:flex-row items-start justify-between md:items-center mb-7 gap-y-6 md:gap-y-4 lg:mb-16"
+>
   <!-- SEARCH FIEL  -->
   <Search />
 
   <!-- NEW INVOICE BUTTON  -->
   <div>
     <button
-      class="relative whitespace-normal rounded-lg bg-lavenderIndigo px-10 py-3 font-sansSerif text-xl font-black text-white shadow-colored hover:shadow-coloredHover translate-y-0 hover:-translate-y-2"
+      class="relative whitespace-normal rounded-lg bg-lavenderIndigo px-5 lg:px-10 py-2 lg:py-3 font-sansSerif text-base lg:text-xl font-black text-white shadow-colored hover:shadow-coloredHover translate-y-0 hover:-translate-y-2"
       >+Invoice</button
     >
   </div>
@@ -26,7 +28,7 @@
 <!-- LIST OF INVOICES  -->
 <div>
   <!-- HEADER -->
-  <div class="table-header invoice-table text-daisyBush">
+  <div class="table-header invoice-table text-daisyBush hidden lg:grid">
     <h3>Status</h3>
     <h3>Due Date</h3>
     <h3>Id</h3>
@@ -37,16 +39,18 @@
   </div>
 
   <!-- INVOICES -->
-  <div class="invoice-table items-center bg-white py-6 rounded-lg shadow-tableRow">
-    <div><Tag label="sent" /></div>
-    <div class="text-lg">8/1/2022</div>
-    <div class="text-lg">1234</div>
-    <div class="text-xl font-bold">Compressed firm</div>
-    <div class="text-lg font-mono font-bold">$45</div>
-    <div class="text-lg center">
+  <div
+    class="invoice-table invoice-row items-center bg-white py-3 lg:py-6 rounded-lg shadow-tableRow"
+  >
+    <div class="status"><Tag className="ml-auto lg:ml-0" label="sent" /></div>
+    <div class="text-sm lg:text-lg dueDate">8/1/2022</div>
+    <div class="text-sm lg:text-lg invoiceNumber">1234</div>
+    <div class="text-base lg:text-xl font-bold clientName">Compressed firm</div>
+    <div class="text-sm lg:text-lg font-mono font-bold amount text-right">$45</div>
+    <div class="text-sm lg:text-lg viewButton hidden lg:block center">
       <a href="#" class="text-pastelPurple hover:text-daisyBush"><View /></a>
     </div>
-    <div class="text-lg center">
+    <div class="text-lg center moreButton hidden lg:block">
       <button class=" text-pastelPurple hover:text-daisyBush"><ThreeDots /></button>
     </div>
   </div>
@@ -57,5 +61,46 @@
 <style lang="postcss">
   .table-header h3 {
     @apply text-xl font-black leading-snug;
+  }
+
+  .invoice-row {
+    grid-template-areas:
+      'invoiceNumber invoiceNumber'
+      'clientName amount'
+      'dueDate status';
+  }
+
+  @media (min-width: 1024px) {
+    .invoice-row {
+      grid-template-areas: 'status dueDate invoiceNumber clientName amount viewButton moreButton';
+    }
+  }
+
+  .invoice-row .status {
+    grid-area: status;
+  }
+
+  .invoice-row .dueDate {
+    grid-area: dueDate;
+  }
+
+  .invoice-row .invoiceNumber {
+    grid-area: invoiceNumber;
+  }
+
+  .invoice-row .clientName {
+    grid-area: clientName;
+  }
+
+  .invoice-row .amount {
+    grid-area: amount;
+  }
+
+  .invoice-row .viewButton {
+    grid-area: viewButton;
+  }
+
+  .invoice-row .moreButton {
+    grid-area: moreButton;
   }
 </style>

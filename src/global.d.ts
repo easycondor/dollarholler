73cb03063147953ff077171d4276b49cc653d5e9
SvelteCondor <SvelteCondor@gmail.com>
2023-04-15 19:18:19 +0200

    TS global.d.ts Enums

---
+++ src/global.d.ts
@@ -0,0 +1,32 @@
+
+interface Invoice {
+    client: Client
+    createAt: string
+    dueDate: string
+    invoiceNumber: string
+    invoiceStatus: InvoiceStatus
+    issueDate: string
+    lineItems?: LineItem[]
+    notes?: string
+    subject?: string
+    terms?: string
+}
+
+interface Client {
+    city: string
+    clientStatus: CllientStatus
+    email: string
+    id: string
+    name: string
+    state: string
+    street: string
+    zip: string
+}
+
+interface LineItem {
+    amount: string
+    description: string
+    id: string
+    quantity: number
+}
+
